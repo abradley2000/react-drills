bcf7f454719b801ee8fdf1d01a3bb78dd2cdb1fc
abradley2000 <aaron.s.bradley2000@gmail.com>
2020-12-26 17:35:05 -0600

    Completed app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filter: '',
+      foodArray: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    }
+  }
+  changeFilter(e) {
+    this.setState({
+      filter: e
+    })
+  }
+  render() {
+    const listItems = this.state.foodArray.filter(el => el.includes(this.state.filter)).map(el => <h2>{el}</h2>)
+    return (
+      <div className="App">
+        <input onChange={(e) => this.changeFilter(e.target.value)}></input>
+        <p>{listItems}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
